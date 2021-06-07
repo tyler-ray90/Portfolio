33cf8f14190ccbb0da81c782ca0c3356e34b82a4
tyler-ray90 <tyler_ray98@yahoo.com>
2021-06-07 15:07:03 -0400

    Adjusted picture dimensions and added title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
     <div className="App">
         <h1>tyler ray</h1>
         <img className="Profile" alt="Profile"src={Profile} />
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eaque ducimus, at veniam minus aut explicabo? Voluptatibus ducimus culpa vitae, minima aperiam, eum dignissimos unde minus nesciunt ex nulla neque.</h2>
+        <h2>As a software engineer I eagerly seek challenges I can solve that will produce efficient results. Staying relevant, consistent self-education, and solving complex problems are at the core of what keeps me passionate and motivated to thrive in the tech industry. With a rich background in hospitality and communication, I help bridge the gap between technology and its users.
+        <br/>
+        <br/>
+        If you can confidently talk to people about software, you can most certainly talk to a computer.</h2>
         <footer>Copyright Tyler Ray 2021</footer>
       
     </div>
