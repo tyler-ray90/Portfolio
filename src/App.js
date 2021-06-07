60e9cc5eb52417a25f0c6842ebb2c0a69b4b4d5a
tyler-ray90 <tyler_ray98@yahoo.com>
2021-06-06 22:46:34 -0400

    Added the footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
         <h1>tyler ray</h1>
         <img className="Profile" alt="Profile"src={Profile} />
-        
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eaque ducimus, at veniam minus aut explicabo? Voluptatibus ducimus culpa vitae, minima aperiam, eum dignissimos unde minus nesciunt ex nulla neque.</h2>
+        <footer>Copyright Tyler Ray 2021</footer>
       
     </div>
   );
