5a9085c328773fc32ca88f432a4118773bb7cfde
tyler-ray90 <tyler_ray98@yahoo.com>
2021-06-08 14:05:33 -0400

    Updated Portfolio page

--- src/Pages/Portolio/Portfolio.js
+++ src/Pages/Portolio/Portfolio.js
@@ -2,7 +2,11 @@ function Portfolio(props) {
     return (
         <div className="projects">
         <a href="https://low-rent.netlify.app/">Low-Rent</a>
+        <a href="https://self-journal.netlify.app/">Self Journal</a>
         <a href="https://game-review-project.herokuapp.com/">MetaReviews</a>
+        <a href="https://pokefinderr.netlify.app/">PókeFinder</a>
+
+
 
         </div>
     )
