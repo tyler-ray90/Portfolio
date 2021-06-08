991244a37dfd7c38641731eac06cdb4d5bd71c1a
tyler-ray90 <tyler_ray98@yahoo.com>
2021-06-08 15:51:41 -0400

    Updated resume link

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -4,7 +4,7 @@ function Contact(props) {
         <div className="contact-info">
             <p>Phone: (603)-933-0011</p>
             <p>Email: Tyler.P.Ray@icloud.com</p>
-            <a href="https://docs.google.com/document/d/14xNfGm50k6cpZr51aoT7fyjdqEn42MwU/edit">Resume</a>
+            <a href="https://docs.google.com/document/d/e/2PACX-1vQkgRlsO7pAsVR-N0mB3dKc52KpfD--8KQytBTDh2jzW44A5hnXUhNZKl5v1X_Q0A/pub">Resume</a>
             
             
         </div>
