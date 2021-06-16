8f5fa8cd18d2661ee5e02041211b17d263d5f31b
tyler-ray90 <tyler_ray98@yahoo.com>
2021-06-16 15:45:22 -0400

    Added new project to portfolio

--- src/Pages/Portolio/Portfolio.js
+++ src/Pages/Portolio/Portfolio.js
@@ -3,6 +3,7 @@ function Portfolio(props) {
         <div className="projects">
         <a href="https://low-rent.netlify.app/">Low-Rent</a>
         <a href="https://self-journal.netlify.app/">Self Journal</a>
+        <a href="https://cinema-search-app.netlify.app/">Cinema Search</a>
         <a href="https://game-review-project.herokuapp.com/">MetaReviews</a>
         <a href="https://pokefinderr.netlify.app/">PókeFinder</a>
 
